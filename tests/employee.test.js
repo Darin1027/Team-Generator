558d042d76530085616e8ac39ac51652c90f9372
DareEnd <114109917+Darin1027@users.noreply.github.com>
2022-11-25 23:10:55 -0700

    saving changes, working on employee test.js and employeeClass.js

---
+++ tests/employee.test.js
@@ -0,0 +1,38 @@
+// import employee class
+const employee = require("../lib/employeeClass");
+// start test
+test("test new employee", ()) => {
+    expect()
+};
+
+
+
+
+
+
+
+
+
+
+
+
+// class employee {
+//     constructor(name, id, email) {
+//       this.name = name;
+//       this.id = id;
+//       this.email = email;
+//     }
+  
+//     getName() {
+//       return this.name;
+//     }
+//     getId() {
+//       return this.id;
+//     }
+//     getEmail() {
+//       return this.email;
+//     }
+//     getRole() {
+//       return "employee";
+//     }
+//   }
